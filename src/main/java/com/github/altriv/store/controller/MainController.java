41e150d72065bf856401091dc3ca49e52b725409
Aliaksei Trynda <aliaksei.trynda@gmail.com>
2025-09-15 03:48:50 +0300

    Fix redirect from root path

--- src/main/java/com/github/altriv/store/controller/MainController.java
+++ src/main/java/com/github/altriv/store/controller/MainController.java
@@ -13,14 +13,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import reactor.core.publisher.Mono;
 
 @Slf4j
 @Controller
-@RequestMapping("/")
 @RequiredArgsConstructor
 public class MainController {
 
